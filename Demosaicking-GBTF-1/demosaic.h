7da26deff7a17949467a101985c91aa817ecbfb1
Wanted2253 <80056095+Wanted2253@users.noreply.github.com>
2021-08-20 18:11:31 +0530

    Add project files.

---
+++ Demosaicking-GBTF-1/demosaic.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include "gbtf.h"
+using namespace std;
+using namespace cv;
+void Demosaic(cv::Mat& img, cv::Mat& Dst, int BayerPatternFlag);
